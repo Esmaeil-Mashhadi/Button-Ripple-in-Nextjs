43c088c1a273582d8b7d5ff64db72abd75f0177d
Esmeail Mashhadi <codealchemy.esi@gmail.com>
2023-11-12 16:36:15 +0330

    button effect ready

--- app/page.js
+++ app/page.js
@@ -1,95 +1,59 @@
-import Image from 'next/image'
+"use client"
 import styles from './page.module.css'
+import { createElement, useState } from 'react';
+import { useEffect } from "react";
+import React from 'react';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const RefPage = () => {
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+     const [elements, setElements] = useState([]);
+     const [btnclick , setBtnclick] = useState({x:0 , y:0})
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+    const clickHandler=(e)=>{
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+     const {clientX , clientY} = e
+       const rect = e.target.getBoundingClientRect()
+       console.log(rect);
+       const xResult = clientX - rect.left;
+       const yResult = clientY - rect.top;
+
+    setBtnclick({
+        x:xResult , y:yResult
+    })
+    
+    
 }
+
+
+console.log(
+    elements
+);
+
+
+
+     const {x , y} = btnclick
+
+    useEffect(()=>{
+        if(btnclick.x && btnclick.y){
+            const newElement = createElement("span", {className:`${styles.show}`, style:{left:`${x}px` , top:`${y}px`}}) 
+    setElements([...elements, newElement]); 
+        }
+       
+    },[btnclick])
+    
+
+    return (
+        <div className={styles.container}>
+            <button onClick={clickHandler}  className={styles.btn}>click Me
+              {elements.map((item , index)=>{
+                return <React.Fragment key={index}>{item}</React.Fragment>
+              })}
+            </button>
+
+        </div>
+    );
+};
+
+export default RefPage;
